80eeaf93da87c469101222a38421c76e10da6b4b
alanoodalhedban <alanoodalhedban@gmail.com>
2021-05-23 22:55:20 +0300

    add file

---
+++ app.js
@@ -0,0 +1,19 @@
+let userWeight= prompt('How much do you weight ?')
+
+    console.log(userWeight) 
+
+
+    let userLength = prompt('How much do you Length ?')
+
+    console.log(userLength ) 
+
+
+    let userdrink= prompt('what is your favorite drink?')
+
+    console.log(userdrink) 
+
+    let timebreak= prompt('What time do you have breakfast? ?')
+
+    console.log(timebreak) 
+    
+    alert('my weight'+ userWeight+ 'my lenght'+ userLength+ 'my favorite drink is'+ userdrink + 'I have breakfast at '+ timebreak)
